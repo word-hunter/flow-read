24c0c9a440107534da2b3f7559e1469b73ece8f9
sunnylost <sunnylost@gmail.com>
2026-05-20 22:56:01 +0800

    chore(release): 发布 alpha 0.0.3

--- lib/services/app_version.dart
+++ lib/services/app_version.dart
@@ -1,10 +1,10 @@
 class FlowReadVersion {
-  static const name = '0.0.2';
+  static const name = '0.0.3';
   static const channel = 'alpha';
-  static const releaseName = '0.0.2-alpha';
-  static const buildNumber = 2;
-  static const full = '0.0.2+2';
-  static const tag = 'v0.0.2-alpha';
-  static const shortDisplay = 'alpha 0.0.2';
-  static const display = 'alpha 0.0.2 (2)';
+  static const releaseName = '0.0.3-alpha';
+  static const buildNumber = 3;
+  static const full = '0.0.3+3';
+  static const tag = 'v0.0.3-alpha';
+  static const shortDisplay = 'alpha 0.0.3';
+  static const display = 'alpha 0.0.3 (3)';
 }
