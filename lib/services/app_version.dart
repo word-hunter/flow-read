b96985fef34ad17297f301d6da040af3dc72cc78
sunnylost <sunnylost@gmail.com>
2026-05-31 14:52:47 +0800

    chore(release): 发布 1.4.0

--- lib/services/app_version.dart
+++ lib/services/app_version.dart
@@ -1,10 +1,10 @@
 class FlowReadVersion {
-  static const name = '1.3.0';
+  static const name = '1.4.0';
   static const channel = 'stable';
-  static const releaseName = '1.3.0';
-  static const buildNumber = 8;
-  static const full = '1.3.0+8';
-  static const tag = 'v1.3.0';
-  static const shortDisplay = '1.3.0';
-  static const display = '1.3.0 (8)';
+  static const releaseName = '1.4.0';
+  static const buildNumber = 9;
+  static const full = '1.4.0+9';
+  static const tag = 'v1.4.0';
+  static const shortDisplay = '1.4.0';
+  static const display = '1.4.0 (9)';
 }
